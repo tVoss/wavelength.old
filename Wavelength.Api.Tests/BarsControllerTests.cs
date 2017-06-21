6b005bc634d22ae628ba78575ed686675b0f8739
Tyler Voss <tdvoss2@illinois.edu>
2017-06-21 17:03:04 -0500

    Moved stuff around

---
+++ Wavelength.Api.Tests/BarsControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Wavelength.Api.Controllers;
+
+namespace Wavelength.Api.Tests
+{
+    [TestClass]
+    public class BarsControllerTests
+    {
+
+        public void Idk()
+        {
+            var ops = new DbContextOptionsBuilder().UseMemoryCache(new MemoryCache(null)).Options;
+            WavelengthDbContext ctx = new WavelengthDbContext(ops);
+            var ctrl = new BarsController(ctx, null);
+        }
+    }
+}
